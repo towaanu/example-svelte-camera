c9e959fc2115ee7e0c5afe9c68c3bf95d302eed5
Antoine Muller <niantoinesan@gmail.com>
2020-07-26 11:14:10 +0200

    add take photo feature

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -2,11 +2,4 @@
     import Camera from './components/Camera/Camera.svelte'
 </script>
 
-<style>
-    h1 {
-        color: blue;
-    }
-</style>
-
-<h1>Hello world !</h1>
 <Camera />
--- app/src/components/Camera/Camera.svelte
+++ app/src/components/Camera/Camera.svelte
@@ -1,15 +1,71 @@
 <script>
+    import {onMount} from 'svelte'
     import CameraCapture from './CameraPreview.svelte'
-    
-    import { getMediaStream } from '../../helpers/cameraHelper'
+    import { getMediaStream, getImageCapture } from '../../helpers/cameraHelper'
 
-    const mediaStreamPromise = getMediaStream()
+
+    const cameraWidth = 500
+    const cameraHeight = 500
+
+    let pictureUrl = null
+    let imageCapture = null
+    let mediaStreamPromise = null
+
+    onMount(() => {
+        mediaStreamPromise = getMediaStream({video: true})
+            .then(mediaStream => {
+                imageCapture = getImageCapture(mediaStream)
+                return mediaStream
+            })
+    })
+
+    function takePicture(){
+        imageCapture.takePhoto()
+            .then(blob => {
+                pictureUrl = URL.createObjectURL(blob)
+            })
+            .catch(err => console.log("Error while taking photo ", err))
+    }
+
 </script>
 
-{#await mediaStreamPromise}
-    <h1>Waiting for camera....</h1>
-{:then mediaStream}
-    <CameraCapture mediaStream={mediaStream} />
-{:catch error}
-    <h1>Error: {error.message}</h1>
-{/await}
+<style>
+    .camera {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .camera-video {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .taken-picture {
+        object-fit: contain;
+        border: 5px solid brown;
+    }
+
+    .take-picture-btn {
+        align-self: center;
+    }
+</style>
+
+<div class="camera">
+    <div class="camera-video">
+        {#await mediaStreamPromise}
+            <h1>Waiting for camera....</h1>
+        {:then mediaStream}
+            <CameraCapture mediaStream={mediaStream} previewWidth={cameraWidth} previewHeight={cameraHeight}/>
+        {:catch error}
+            <h1>Error: {error.message}</h1>
+        {/await}
+
+        {#if pictureUrl}
+            <img class="taken-picture" src={pictureUrl} alt="result" height={cameraHeight} width={cameraWidth}/>
+        {/if}
+    </div>
+    <button class="take-picture-btn" on:click={takePicture}>Take picture !</button>
+</div>
