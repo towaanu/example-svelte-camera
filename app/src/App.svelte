5aabd305a6f304261b67196b483402b88412a227
Antoine Muller <niantoinesan@gmail.com>
2020-07-26 09:40:40 +0200

    create Camera component

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,9 +1,5 @@
 <script>
-    import CameraCapture from './CameraPreview.svelte'
-    
-    import { getMediaStream } from './helpers/cameraHelper'
-
-    const mediaStreamPromise = getMediaStream()
+    import Camera from './components/Camera/Camera.svelte'
 </script>
 
 <style>
@@ -13,11 +9,4 @@
 </style>
 
 <h1>Hello world !</h1>
-
-{#await mediaStreamPromise}
-    <h1>Waiting for camera....</h1>
-{:then mediaStream}
-    <CameraCapture mediaStream={mediaStream}/>
-{:catch error}
-    <h1>Error: {error.message}</h1>
-{/await}
+<Camera />
---
+++ app/src/components/Camera/Camera.svelte
@@ -0,0 +1,15 @@
+<script>
+    import CameraCapture from './CameraPreview.svelte'
+    
+    import { getMediaStream } from '../../helpers/cameraHelper'
+
+    const mediaStreamPromise = getMediaStream()
+</script>
+
+{#await mediaStreamPromise}
+    <h1>Waiting for camera....</h1>
+{:then mediaStream}
+    <CameraCapture mediaStream={mediaStream} />
+{:catch error}
+    <h1>Error: {error.message}</h1>
+{/await}
